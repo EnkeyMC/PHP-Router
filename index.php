88c18f301afd98feb6c1220abb7d9f6f8c572ca7
EnkeyMC <enkeymc@gmail.com>
2017-04-08 22:58:10 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
 require_once 'router/Router.php';
+require_once 'router/base_controller/BaseController.php';
+require_once 'test_controllers/controller1.php';
+
+Router::registerControllers(['test' => Controller1::class]);
+Router::registerRoute('/<controller:test>/<task>/');
+Router::registerRoute('/<controller>/<view>/<id>');
+
+Router::route();
+
 ?>
