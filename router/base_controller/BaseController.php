aea7605acdcffad0e4d49654f9fc3517c94fa069
EnkeyMC <enkeymc@gmail.com>
2017-04-08 22:21:37 +0200

    Base controller

---
+++ router/base_controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * @package PHP-Router
+ * @author Martin Omacht
+ * @date 8.4.2017
+ */
+
+/**
+* Base class for Controllers
+*/
+class BaseController
+{
+	public function __construct()
+	{
+		
+	}
+
+	/**
+	 * Router calls this function if requested
+	 * @param  array $params Associative array of other parameters
+	 */
+	protected function invoke(array $params)
+	{
+		
+	}
+}
